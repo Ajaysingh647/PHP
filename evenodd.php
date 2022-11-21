5b67c047294a3091c5462ce99e898ffba38c0ba1
Ajay singh <ajaysingh12861@gmail.com>
2022-11-21 10:44:46 +0530

    Add files via upload

---
+++ evenodd.php
@@ -0,0 +1,11 @@
+<?php
+echo "This program use to check the given array is odd or even\r\n";
+$num1 = 13;
+if($num1%2==0){
+    echo "The given number is EVEN";
+}
+else{
+        echo "The given number is ODD";
+    }
+
+?>
